bbaf459163ae179ac5fd20d7ef5325ee1f493b49
luixaviles <luis.avilesz@gmail.com>
2016-10-21 23:51:04 -0400

    Updating title of the app

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -176,7 +176,7 @@ export class SeedConfig {
    * `index.html`.
    * @type {string}
    */
-  APP_TITLE = 'Welcome to angular2-seed!';
+  APP_TITLE = 'Angular 2 Tech Event Application';
 
   /**
    * The base folder of the applications source files.
